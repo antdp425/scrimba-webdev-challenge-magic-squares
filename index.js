f967c0e307e453736eef6fdc444a045b13a3ccb8
antdp425 <antdp425@aol.com>
2020-12-03 13:52:03 -0500

    Adds starter files

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+In this challenge you are a magician that tells your audience that by pressing only
+the first square on the left, you can change ALL 9 squares on the grid to be yellow.
+Write a function using the .forEach() method that will do just that.
+
+event listeners, Array work
+
+*/
+
+// Write your code here 👇
+    
+    
+
+
+/*
+
+DETAILED INSTRUCTIONS 
+1. Use JavaScript to turn elements into an Array
+2. Use the .forEach() method to apply a change to all the elements
+3. Listen out for when the first square is clicked
+
+STRETCH GOALS:
+- What else can you change about the squares?
+- Can you improve the overall design?
+
+*/
